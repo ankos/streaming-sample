bd2a463a2e358730ee220d760d275990aa1b465f
Andre Koser <andre.koser@msg-gillardon.de>
2019-06-02 15:58:14 +0200

    Update stream output

--- src/main/java/streaming/sample/StreamingController.java
+++ src/main/java/streaming/sample/StreamingController.java
@@ -4,14 +4,15 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.reactivex.Flowable;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Controller("/api")
 public class StreamingController {
 
-    @Get(value = "/values", processes = "text/csv")
+    @Get(value = "/et-list", processes = "text/csv")
     public Flowable<String> text() {
-        return Flowable.range(1, 1000).map(x -> LocalDateTime.now().toString() + ",comment + " + x + "\n");
+        return Flowable.range(1, 1000).map(x -> LocalDate.now().plusYears(x) + ", attending: yes\n");
     }
 
 }
